6ade9bc997ecfe420d426d1134c5ba24b48eb12f
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-12-01 09:56:24 +0100

    Setup airbnb & airbnb-typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
-    "env": {
-        "es2021": true
+    env: {
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'airbnb',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    plugins: [
+        'react',
+        '@typescript-eslint',
     ],
-    "rules": {
-    }
-}
+    rules: {},
+};
