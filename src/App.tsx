0f647ce425c0d23f9d3e75154381400e1cf5fd29
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-12-05 10:54:34 +0100

    Create NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
