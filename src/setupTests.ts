b97b058528649edd12cd65c971defb14f1108b5b
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-12-05 10:05:34 +0100

    Configure matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,7 @@
-// Specific setups that are runned before any test
+/* eslint-disable import/no-extraneous-dependencies */
+// Specific setups that are runned before any test is executed.
+
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+
+expect.extend(matchers);
