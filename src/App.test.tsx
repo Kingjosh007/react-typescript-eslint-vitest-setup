537134cee15c68d25f173dc079bf1e382f0d5cd3
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-12-05 11:45:42 +0100

    Test NotFound page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,35 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
-import App from './App';
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
   it('Renders Hello World', () => {
     // ARRANGE
-    render(<App />);
+    render(<WrappedApp />);
     // ACT
-    // EXPECT
+    // ASSERT / EXPECT
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
     ).toHaveTextContent('Hello World');
+  });
+
+  it('Renders Not Found if invalid path', () => {
+    // ARRANGE
+    render(
+      <MemoryRouter initialEntries={['/banana']}>
+        <App />
+      </MemoryRouter>
+    );
+    // ACT
+    // ASSERT / EXPECT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
   });
 });
